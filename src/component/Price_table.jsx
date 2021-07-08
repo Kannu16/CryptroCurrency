916baef739e84a861fff8fd89796a98de55f235f
kannu_16 <83232786+Kannu16@users.noreply.github.com>
2021-07-08 10:50:12 +0530

    Update Price_table.jsx

--- src/component/Price_table.jsx
+++ src/component/Price_table.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import pData from '../component/Data';
 
 const Price_table = () => {
     return (
@@ -14,57 +15,20 @@ const Price_table = () => {
     </tr>
   </thead>
   <tbody>
-    <tr className="table-row">
-      <th scope="row">1</th>
-      <td>WazirX</td>
-      <td>₹ 26,41,497</td>
-      <td>₹ 26,39,812 / ₹ 26,41,497</td>
-      <td style={{color:"#00c2cb"}}>0.21 %</td>
-      <td style={{color:"#00c2cb"}}> ▲ ₹ 5,615 </td>
-    </tr>
-    <tr className="table-row">
-      <th scope="row">2</th>
-      <td>Bitbns</td>
-      <td>₹ 26,52,709</td>
-      <td>₹ 26,48,943 / ₹ 26,52,709</td>
-      <td style={{color:"#00c2cb"}}>0.64 %</td>
-      <td style={{color:"#00c2cb"}}>▲ ₹ 16,828</td>
-    </tr>
-    <tr className="table-row">
-      <th scope="row">3</th>
-      <td>Giotus</td>
-      <td>₹ 26,46,000</td>
-      <td>₹ 26,44,500 / ₹ 26,93,000</td>
-      <td style={{color:"#00c2cb"}}>0.38 %</td>
-      <td style={{color:"#00c2cb"}}>▲ ₹ 10,118</td>
-    </tr>
-
-    <tr className="table-row">
-      <th scope="row">4</th>
-      <td>Colodax</td>
-      <td>₹ 25,83,138</td>
-      <td>₹ 25,75,167 / ₹ 26,08,984</td>
-      <td className="text-danger">-2.00 %</td>
-      <td className="text-danger">▼ ₹ 52,743</td>
-    </tr>
-
-    <tr className="table-row">
-      <th scope="row">5</th>
-      <td>Zebpay</td>
-      <td>₹ 26,53,990</td>
-      <td>₹ 26,53,990 / ₹ 26,52,000</td>
-      <td style={{color:"#00c2cb"}}>0.69 %</td>
-      <td style={{color:"#00c2cb"}}>▲ ₹ 18,108</td>
-    </tr>
-
-    <tr className="table-row">
-      <th scope="row">6</th>
-      <td>CoinDcx</td>
-      <td>₹ 26,37,954</td>
-      <td>₹ 26,37,959 / ₹ 26,50,126</td>
-      <td style={{color:"#00c2cb"}}>0.08 %</td>
-      <td style={{color:"#00c2cb"}}>▲ ₹ 2,073</td>
-    </tr>
+      {
+    pData.map((currElem,index)=>{
+      return(
+      <tr className="table-row" key={index}>
+        <td style={{color:"grey"}}>{currElem.id}</td>
+        <td>{currElem.Platform}</td>
+        <td>{currElem.LastTradedPrice}</td>
+        <td>{currElem.BuySellPrice}</td>
+        <td style={{color:"#00c2cb"}}>{currElem.difference}</td>
+        <td style={{color:"#00c2cb"}}>{currElem.savings}</td>
+      </tr>
+      )
+    })
+  }
   </tbody>
 </table>
     )
